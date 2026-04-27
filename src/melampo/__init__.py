c0d5fab5cf99dfabbe8edba8e2b6c283d1b38856
tablet-ax3-ops <tablet.tester.user93@gmail.com>
2026-04-27 14:19:47 +0200

    export prototype runner from package

--- src/melampo/__init__.py
+++ src/melampo/__init__.py
@@ -1,5 +1,11 @@
 """Melampo package."""
 
 from .app import build_default_runtime
+from .prototype import ClinicalPrototypeRunner, PrototypeInputValidator, run_prototype_case
 
-__all__ = ["build_default_runtime"]
+__all__ = [
+    "build_default_runtime",
+    "ClinicalPrototypeRunner",
+    "PrototypeInputValidator",
+    "run_prototype_case",
+]
